90541f718bd530402936350d810dd2f10496d650
Kevin Weekly <kweekly@eecs.berkeley.edu>
2011-03-29 22:58:26 -0700

    Path labelling

--- openopt/path_process.py
+++ openopt/path_process.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 import os,sys
 import Image
+from random import random
 import ImageFilter, ImageDraw
 
 img = Image.open("rivers.png");
 pix = img.load();
 
-pathimg = Image.new('L',img.size,255);
+pathimg = Image.new('RGB',img.size,(255,255,255));
 pathpx = pathimg.load()
 pathdraw = ImageDraw.Draw(pathimg)
+
+colors = [(128,0,0),(0,128,0),(0,0,128),(128,128,0),(0,128,128),(128,0,128)];
+
 
 greens = [];
 reds = [];
@@ -28,75 +32,63 @@ for y in range(0,img.size[1]):
 
 gx,gy = greens[0];
 i = 0;
+pix[gx,gy] = (0,255,0);
+used_labels = [0];
+edge = [(gx,gy,0)];
+newedge = [];
+connections = [];
+while edge:
+    for (x, y, l) in edge:
+        for (s, t) in ((x+1, y), (x-1, y), (x, y+1), (x, y-1),(x+1,y+1),(x-1,y+1),(x+1,y-1),(x-1,y-1)):
+            if s >= 0 and t >= 0 and s < img.size[0] and t < img.size[1] and \
+                pix[s,t][1] != 255 and pix[s,t][0] == 0:
+                pix[s,t] = (0,255,0);
+                newedge.append((s, t, l))
+                
+    edge = []
 
-for xd in [-1,0,1]:
-    for yd in [-1,0,1]:
-        pix[gx+xd,gy+yd] = (0,255,0);
+    #print "Iteration %d:"%i
+    p = 0;
+    labels_seen = [];
+    while newedge:
+        (x,y,l) = newedge.pop();
 
-pix[gx,gy] = (0,128,0);
-
-img.save("frames/%04d.png" % i);
-i += 1;
-
-done = False;
-while not done:
-    lastimg = img;
-    lastpix = pix;
-    
-    img = img.copy();
-    pix = img.load();
+        if ( l in labels_seen ): # already saw this label, but not a part of it, a new edge is born!
+            old = l;
+            while l in used_labels:
+                l += 1;
+            used_labels.append(l);
+            connections.append((old,l)); # split
+                    
+        labels_seen.append(l);
         
-    done = True
-    for y in range(0,img.size[1]):
-        for x in range(0,img.size[0]):
-            if ( lastpix[x,y][1] == 255 and lastpix[x,y][0] == 0):
-                pix[x,y] = (0,128,0);
-                for xd in [-1,0,1]:
-                    for yd in [-1,0,1]:
-                        if ( x+xd > 0 and x+xd < img.size[0] and y+yd > 0 and y + yd < img.size[1] and lastpix[x+xd,y+yd][1] == 0 ):
-                            pix[x+xd,y+yd] = (0,255,0);
-                            done = False
-    print i
-    img.save("frames/%04d.png" % i);
-                       
-    """for y in range(0,img.size[1]):
-        for x in range(0,img.size[0]):
-            if pix[x,y][1] == 255 and pix[x,y][0] == 0 :
-                pix[x,y] = (0,128,0);
-    """            
+        edge.append((x,y,l));
+        tocheck = [(x,y,l)];
+        thisedge = [(x,y,l)];
+        while tocheck:
+            (cx,cy,l) = tocheck.pop();
+            for (mx, my) in ((cx+1, cy), (cx-1, cy), (cx,cy+1), (cx, cy-1)):
+                for (ox,oy,ol) in newedge:
+                    if ( ox == mx and oy == my):
+                        newedge.remove((ox,oy,ol));
+                        thisedge.append((ox,oy,l));
+                        tocheck.append((ox,oy,l));
+                        edge.append((ox,oy,l));
+                        if ( ol != l and (ol,l) not in connections) :
+                            connections.append((ol,l)); #join
+                               
+        for (x,y,l) in thisedge:
+            pathpx[x,y] = (5*l,5*l,5*l);
+        
+        p += 1;
     
+   # pathimg.save("frames/%04d.png"%i);
     i += 1;
+    
 
-"""
-inwater = False;
-chans = []
-watstartx = 0;
+pathimg.show();
+print connections;
 
-for y in range(0,img.size[1]):
-    numchans = 0;
-    for x in range(0,img.size[0]):
-        if not inwater and pix[x,y][0] == 0:
-            inwater = True;
-            watstartx = x;
-        elif inwater and pix[x,y][0] == 255:
-            inwater = False;
-            numchans += 1
-    if inwater:
-        pathimg[int((x+img.size[0])/2),y] = 0;
-        numchans += 1
-    chans.append(numchans);
-
-for y in range(1,len(chans)-1):
-    if ( chans[y] > chans[y-1] and chans[y+1] >= chans[y] ):
-        for x in range(0,img.size[0]):
-            pix[x,y] = (0,255,0);
-    elif ( chans[y] < chans[y-1] and chans[y+1] <= chans[y]):
-        for x in range(0,img.size[0]):
-            pix[x,y] = (255,0,0);
-"""     
-
-img.show();
-
-print "reds",reds
-print "greens",greens
+#print "reds",reds
+#print "greens",greens
 # Use blue band for river
