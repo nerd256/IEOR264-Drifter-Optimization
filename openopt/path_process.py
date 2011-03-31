be925b069a77d97a1d5e2dbae069a50348441d2f
Kevin Weekly <kweekly@eecs.berkeley.edu>
2011-03-30 20:49:04 -0700

    Merging of redundant paths (Doesn't work

--- openopt/path_process.py
+++ openopt/path_process.py
@@ -11,8 +11,21 @@ pathimg = Image.new('RGB',img.size,(255,255,255));
 pathpx = pathimg.load()
 pathdraw = ImageDraw.Draw(pathimg)
 
-colors = [(128,0,0),(0,128,0),(0,0,128),(128,128,0),(0,128,128),(128,0,128)];
 
+#colors = [(128,0,0),(0,128,0),(0,0,128),(128,128,0),(0,128,128),(128,0,128)];
+colors = [];
+for i in range(0,1000):
+    colors.append((random()*256,random()*256,random()*256));
+
+def disp_labs():
+    dispimg = Image.new('RGB',img.size,(255,255,255));
+    disppx = dispimg.load();
+    for y in range(0,img.size[1]):
+        for x in range(0,img.size[0]):
+            if ( pathpx[x,y][0] != 255 ):
+                disppx[x,y] = colors[pathpx[x,y][0]];
+    dispimg.show();
+    
 
 greens = [];
 reds = [];
@@ -28,7 +41,12 @@ for y in range(0,img.size[1]):
                 greens.append((x,y));
                 pix[x,y] = (0,0,0);
 
-            #print x,y,pix[x,y]      
+            #print x,y,pix[x,y]
+
+def relabel_edge(thelist, old, new):
+   for j in range(0,len(thelist)):
+        if ( thelist[j][2] == old ):
+          thelist[j] = (thelist[j][0],thelist[j][1],new)
 
 gx,gy = greens[0];
 i = 0;
@@ -47,7 +65,7 @@ while edge:
                 
     edge = []
 
-    #print "Iteration %d:"%i
+#    print "Iteration %d:"%i
     p = 0;
     labels_seen = [];
     while newedge:
@@ -59,6 +77,8 @@ while edge:
             l1 = nextlabel;
             l2 = nextlabel + 1;
             nextlabel += 2;
+            # relabel everything from before
+            relabel_edge(edge, old, l1);
             
             l = l2; # working from second label now
             connections.append((old,l2));
@@ -78,44 +98,110 @@ while edge:
                         thisedge.append((ox,oy,l));
                         tocheck.append((ox,oy,l));
                         edge.append((ox,oy,l));
-                        if ( ol != l and (ol,l) not in connections) :
-                            connections.append((l,ol)); #join
-                               
-        for (x,y,l) in thisedge:
-            pathpx[x,y] = colors[l%len(colors)];#(5*l,5*l,5*l);
-        
+                        if ( ol != l ) :
+                            l1 = nextlabel;
+                            nextlabel += 1;
+                            connections.append((ol,l1)); #join, create new ID and relabel
+                            connections.append((l,l1));
+                            # relabel everything we've processed so far + the new edge
+                            relabel_edge(edge, l, l1);
+                            relabel_edge(tocheck, l, l1);
+                            relabel_edge(newedge, ol, l1);
+                            l = l1;
+                            
+                            
+
         p += 1;
+
+    for (x,y,l) in edge:
+       #pathpx[x,y] = colors[l%len(colors)];
+       pathpx[x,y] = (l,l,l);
     
     #pathimg.save("frames/%04d.png"%i);
     i += 1;
     
 
-#pathimg.show();
-pathimg.save("result.png");
+disp_labs();
+#pathimg.save("result.png");
 
-maxlabel = 0;
-for (il,ol) in connections:
-    if il > maxlabel:
-        maxlabel = il
-    if ol > maxlabel:
-        maxlabel = ol
-    
-pixcnts = [0]*(maxlabel+1);
+pixcnts = [0]*(nextlabel);
+
+print "Edge list:",len(connections);
+connections = sorted(connections);
+used_labels = range(0,nextlabel);
+# remove redundant paths and empty paths
+changed = True;
+relabel = {};
+while changed:
+    changed = False;
+    for i in used_labels:
+        egi = [];
+        for (l1,l2) in connections:
+            if l1 == i:
+                egi.append((i,l2,True));
+            elif l2 == i:
+                egi.append((i,l1,False));
+                
+        if len(egi) == 2:
+            print "Label %d has 2 edges: to %d and %d"%(i,egi[0][1],egi[1][1])
+            changed = True;
+            relabel[i] = egi[0][1]; # arbitrary choice
+            if egi[0][2]:
+                connections.remove((i,egi[0][1]));
+            else:
+                connections.remove((egi[0][1],i));
+            
+            if egi[1][2]:
+                connections.remove((i,egi[1][1]));
+            else:
+                connections.remove((egi[1][1],i));
+
+        elif len(egi) == 0:
+            print "remove",i
+            used_labels.remove(i);
+            changed = True;
+
+# now relabel the image
+for y in range(0,img.size[1]):
+    for x in range(0,img.size[0]):
+        if ( pathpx[x,y][0] != 255 and relabel.has_key(pathpx[x,y][0]) ):
+            v = relabel[pathpx[x,y][0]];
+            pathpx[x,y] = (v,v,v);
+
+disp_labs();
+print used_labels
+# map used labels to 0-index and relabel image
+rlmap = {};
+j = 0;
+for i in used_labels:
+    rlmap[i] = j;
+    j += 1;
+
+# now relabel the image
+for y in range(0,img.size[1]):
+    for x in range(0,img.size[0]):
+        if ( pathpx[x,y][0] == 255):continue;
+        v = rlmap[pathpx[x,y][0]];
+        pathpx[x,y] = (v,v,v);
+
+disp_labs();
+
 """
-#print connections;
 totals = 0;
 for y in range(0,img.size[1]):
     for x in range(0,img.size[0]):
         if pathpx[x,y][0] != 255:
             #print pathpx[x,y]
-            pixcnts[ pathpx[x,y][0]/5 ] += 1
+            pixcnts[ pathpx[x,y][0] ] += 1
             totals += 1
-        
-for i in range(0,maxlabel+1):
-    print "Label %d : %d pixels (%.2f%%)" % (i,pixcnts[i],100*pixcnts[i]/float(totals))
 
-# enumerate routes
 """
+"""        
+for i in range(0,nextlabel):
+    print "Label %d : %d pixels (%.2f%%)" % (i,pixcnts[i],100*pixcnts[i]/float(totals))
+"""
+# enumerate routes
+
 
 
 
